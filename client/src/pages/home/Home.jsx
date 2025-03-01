78448ea6a08c33a806f47f04c5186dd9a63a8001
Visal Vithanage <visalvithanage376@gmail.com>
2025-03-02 01:14:20 +0530

    Update Home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,18 +1,114 @@
 import React from "react";
 import "./Home.scss";
-import Featured from "../../components/featured/Featured";
-import TrustedBy from "../../components/trustedBy/TrustedBy";
-import Slide from "../../components/slide/Slide";
-import CatCard from "../../components/catCard/CatCard";
-import ProjectCard from "../../components/projectCard/ProjectCard";
-import { cards, projects } from "../../data";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { Layout, Pointer, Zap } from "lucide-react";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 
-function Home() {
+
+
+const Home = ({
+  badge = "shadcnblocks.com",
+  heading = "A Collection of Components Built With Shadcn & Tailwind",
+  description = "Join us to build flawless web solutions.",
+  tabs = [
+    {
+      value: "tab-1",
+      icon: <Zap className="h-auto w-4 shrink-0" />,
+      label: "Boost Revenue",
+      content: {
+        badge: "Modern Tactics",
+        title: "Make your site a true standout.",
+        description:
+          "Discover new web trends that help you craft sleek, highly functional sites that drive traffic and convert leads into customers.",
+        buttonText: "See Plans",
+        imageSrc:
+          "https://shadcnblocks.com/images/block/placeholder-dark-1.svg",
+        imageAlt: "placeholder",
+      },
+    },
+    {
+      value: "tab-2",
+      icon: <Pointer className="h-auto w-4 shrink-0" />,
+      label: "Higher Engagement",
+      content: {
+        badge: "Expert Features",
+        title: "Boost your site with top-tier design.",
+        description:
+          "Use stellar design to easily engage users and strengthen their loyalty. Create a seamless experience that keeps them coming back for more.",
+        buttonText: "See Tools",
+        imageSrc:
+          "https://shadcnblocks.com/images/block/placeholder-dark-2.svg",
+        imageAlt: "placeholder",
+      },
+    },
+    {
+      value: "tab-3",
+      icon: <Layout className="h-auto w-4 shrink-0" />,
+      label: "Stunning Layouts",
+      content: {
+        badge: "Elite Solutions",
+        title: "Build an advanced web experience.",
+        description:
+          "Lift your brand with modern tech that grabs attention and drives action. Create a digital experience that stands out from the crowd.",
+        buttonText: "See Options",
+        imageSrc:
+          "https://shadcnblocks.com/images/block/placeholder-dark-3.svg",
+        imageAlt: "placeholder",
+      },
+    },
+  ],
+}) => {
   return (
-    <div className="home">
-      
-    </div>
+    <section className="feature108">
+      <div className="container">
+        <div className="header">
+          <Badge variant="outline">{badge}</Badge>
+          <h1>{heading}</h1>
+          <p>{description}</p>
+        </div>
+        <Tabs defaultValue={tabs[0].value} className="tabs">
+          <TabsList className="tabs-list">
+            {tabs.map((tab) => (
+              <TabsTrigger
+                key={tab.value}
+                value={tab.value}
+                className="tabs-trigger"
+              >
+                {tab.icon} {tab.label}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <div className="tabs-content">
+            {tabs.map((tab) => (
+              <TabsContent
+                key={tab.value}
+                value={tab.value}
+                className="content-grid"
+              >
+                <div className="content-text">
+                  <Badge variant="outline" className="w-fit bg-background">
+                    {tab.content.badge}
+                  </Badge>
+                  <h3>{tab.content.title}</h3>
+                  <p>{tab.content.description}</p>
+                  <Button className="button" size="lg">
+                    {tab.content.buttonText}
+                  </Button>
+                </div>
+                <img
+                  src={tab.content.imageSrc}
+                  alt={tab.content.imageAlt}
+                  className="rounded-xl"
+                />
+              </TabsContent>
+            ))}
+          </div>
+        </Tabs>
+      </div>
+    </section>
   );
-}
+};
 
-export default Home;
+export default Home ;
+
