d182153a62c540cd32de514c8f130fa740f46177
Visal Vithanage <visalvithanage376@gmail.com>
2025-03-03 01:55:37 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,24 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: ['legacy-js-api']
+        silenceDeprecations: ['legacy-js-api'],
+        additionalData: `@import "./src/styles/variables"; @import "./src/styles/mixins";`
       }
     }
-  }
+  },
+  server: {
+    host: true,
+    port: 5173
+  },
 })
