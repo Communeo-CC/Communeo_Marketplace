45c1fcc9cbcaeb8dce4b91becfe03e022761ba50
Visal Vithanage <visalvithanage376@gmail.com>
2025-03-15 17:08:06 +0530

    Update ProjectCard.jsx

--- client/src/components/projectCard/ProjectCard.jsx
+++ client/src/components/projectCard/ProjectCard.jsx
@@ -5,7 +5,6 @@ function ProjectCard({ card }) {
   return (
     <div className="projectCard">
       <img src={card.img} alt="" />
-      {card.category && <span className="category-tag">{card.category}</span>}
       <div className="info">
         <img src={card.pp} alt="" />
         <div className="texts">
